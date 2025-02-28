be400e20478c8f6dd744688086555c0c933fff62
zeroday <zeroday@zero>
2025-03-01 01:13:50 +0200

    Poc Sim

--- Paze/cred.js
+++
@@ -1,3 +0,0 @@
-,\"":[\"\"]}";
-</script>
-<script>window.___webpackCompilationHash="6bedba2546f144f9d239";</script><script src="/webpack-runtime-64cc03370bc58427cf46.js" async></script><script src="/framework-f81141814501d55a471e.js" async></script><script src="/app-673c578dc6f8d9a11459.js" async></script><!-- slice-end id="_gatsby-scripts-1" --></body></html>
--- my_Work/redox/subfinder.sh
+++
@@ -1,3 +0,0 @@
-for url in $(cat /MyFIle/my_Work/redox/main.txt);do
-	subfinder -d $url | tee -a /MyFIle/my_Work/redox/maindomain.txt
-done
